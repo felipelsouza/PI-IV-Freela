031214a32c1d9856f812450dbc3e8a70d2168b31
Felipe Souza <felipelsouza.dev@gmail.com>
2021-04-02 21:29:34 -0300

    Edit index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const cors = require('cors');
+const bodyParser = require('body-parser');
 const routes = require('./routes');
+const cors = require('cors');
 
 require('./database/connection');
 
