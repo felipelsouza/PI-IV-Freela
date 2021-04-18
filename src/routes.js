68dc0b5aa7140b0433c93a80424a0a807856edb3
Felipe Souza <felipelsouza.dev@gmail.com>
2021-04-18 17:56:46 -0300

    Add new routes

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,12 @@ const express = require('express');
 const employerController = require('./controllers/EmployerController');
 const employeeController = require('./controllers/EmployeeController');
 const jobController = require('./controllers/JobController');
+const authController = require('./controllers/AuthController');
 
 const routes = express.Router();
+
+routes.post('/employers/auth', authController.employerLogin);
+routes.post('/employees/auth', authController.employeeLogin);
 
 routes.post('/employers', employerController.create);
 routes.get('/employers', employerController.list);
@@ -19,5 +23,7 @@ routes.put('/employees/:id', employeeController.update);
 routes.delete('/employees/:id', employeeController.destroy);
 
 routes.post('/employers/:employer_id/jobs', jobController.create);
+routes.get('/jobs', jobController.listAll);
+routes.get('/jobs/dev-type/:dev_type', jobController.listByType);
 
 module.exports = routes;
